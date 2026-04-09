4f4209c1e0e9a3c811913b14b9e53b1990f2f1f6
omar kamel <isomardev@gmail.com>
2026-04-09 15:42:17 +0200

    test: add jest setup file

---
+++ packages/backend/tests/setup.ts
@@ -0,0 +1,12 @@
+import { prisma } from '../src/database';
+import { resetDatabase } from './fixtures';
+
+beforeAll(async () => {
+  // Reset database
+  await resetDatabase();
+});
+
+afterAll(async () => {
+  // Disconnect Prisma so Jest can exit cleanly
+  await prisma.$disconnect();
+});
