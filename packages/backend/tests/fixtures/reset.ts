428177206b2c06573df34b82c3c6f55e992f9ece
omar kamel <isomardev@gmail.com>
2026-04-10 19:26:36 +0200

    test: add delete all comments

--- packages/backend/tests/fixtures/reset.ts
+++ packages/backend/tests/fixtures/reset.ts
@@ -2,15 +2,17 @@ import { prisma } from '../../src/database';
 
 export async function resetDatabase() {
   const deleteAllUsers = prisma.user.deleteMany();
-  const deleteAllMember = prisma.member.deleteMany();
+  const deleteAllMembers = prisma.member.deleteMany();
   const deleteAllPosts = prisma.post.deleteMany();
+  const deleteAllComments = prisma.comment.deleteMany();
   const deleteAllVotes = prisma.vote.deleteMany();
 
   try {
     await prisma.$transaction([
       deleteAllUsers,
-      deleteAllMember,
+      deleteAllMembers,
       deleteAllPosts,
+      deleteAllComments,
       deleteAllVotes,
     ]);
   } catch (error) {
