c29405d1510a1a9bbee9b83b1e66e1c348849786
omar kamel <isomardev@gmail.com>
2026-04-12 17:15:30 +0200

    test: add user builder

---
+++ packages/backend/tests/fixtures/builders/user-builder.ts
@@ -0,0 +1,51 @@
+import { faker } from '@faker-js/faker';
+import { prisma } from '../../../src/database';
+import { CreateUserInput } from './create-user-input-builder';
+
+export class UserBuilder {
+  private props: CreateUserInput;
+
+  constructor() {
+    this.props = {
+      email: faker.internet.email(),
+      username: faker.internet.username(),
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      password: faker.internet.password(),
+    };
+  }
+
+  withEmail(email: string = faker.internet.email()) {
+    this.props.email = email;
+    return this;
+  }
+
+  withUsername(username: string = faker.internet.username()) {
+    this.props.username = username;
+    return this;
+  }
+
+  withFirstName(firstName: string = faker.person.firstName()) {
+    this.props.firstName = firstName;
+    return this;
+  }
+
+  withLastName(lastName: string = faker.person.lastName()) {
+    this.props.lastName = lastName;
+    return this;
+  }
+
+  withPassword(
+    password?: string,
+    options?: Parameters<typeof faker.internet.password>[0],
+  ) {
+    this.props.password = password ?? faker.internet.password(options);
+    return this;
+  }
+
+  public async build() {
+    const user = await prisma.user.create({ data: this.props });
+    const { password, ...restUser } = user;
+    return restUser;
+  }
+}
