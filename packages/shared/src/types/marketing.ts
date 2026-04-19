17623ca87e46893b0f5b45475e5c53e5af42f796
omar kamel <isomardev@gmail.com>
2026-04-19 19:24:08 +0200

    feat: add types for email subscription handling in marketing module

---
+++ packages/shared/src/types/marketing.ts
@@ -0,0 +1,11 @@
+import { ApiResponse, GenericErrors } from '.';
+
+export type EmailSubscriber = { email: string };
+
+type EmailNotAddedToMailListError = 'EmailNotAddedToMailList';
+
+export type AddEmailToListErrors = EmailNotAddedToMailListError | GenericErrors;
+export type AddEmailToListResponse = ApiResponse<
+  { subscription: EmailSubscriber },
+  AddEmailToListErrors
+>;
