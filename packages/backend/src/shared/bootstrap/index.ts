9de7c30fd0ac344f8e8b3bf78f57451a82fd64c3
omar kamel <isomardev@gmail.com>
2026-04-23 19:26:47 +0200

    feat: implement bootstrap module for application initialization

---
+++ packages/backend/src/shared/bootstrap/index.ts
@@ -0,0 +1,16 @@
+import { CompositionRoot } from '../composition-root';
+import { Config } from '../config';
+
+const config = new Config('start');
+const compositionRoot = CompositionRoot.createCompositionRoot(config);
+
+const webServer = compositionRoot.getWebServer();
+const db = compositionRoot.getDb();
+
+export const bootstrap = async () => {
+  await webServer.start();
+  await db.connect();
+};
+
+export const app = webServer.getApp();
+export const database = db.getClient();
