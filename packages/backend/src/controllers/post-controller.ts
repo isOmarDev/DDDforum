c173ad2940c192aaa06bb30e3d18ad05d50f083a
omar kamel <isomardev@gmail.com>
2026-04-23 19:24:50 +0200

    refactor: enhance PostController to include error handling setup

--- packages/backend/src/controllers/post-controller.ts
+++ packages/backend/src/controllers/post-controller.ts
@@ -1,13 +1,17 @@
 import express, { Request, Response, NextFunction, Router } from 'express';
 import { PostService } from '../services/post-service';
-import { parseUserForResponse } from '../shared/utils';
+import ErrorExceptionHandler from '../shared/errors/error-exception-handler';
 
 export class PostController {
   private readonly router: Router;
 
-  constructor(private postService: PostService) {
+  constructor(
+    private postService: PostService,
+    private errorExceptionHandler: typeof ErrorExceptionHandler.handle,
+  ) {
     this.router = express.Router();
     this.setupRoutes();
+    this.setupErrorExceptionHandler();
   }
 
   public getRouter() {
@@ -16,6 +20,10 @@ export class PostController {
 
   private setupRoutes() {
     this.router.get('/', this.getPosts.bind(this));
+  }
+
+  private setupErrorExceptionHandler() {
+    this.router.use(this.errorExceptionHandler);
   }
 
   public async getPosts(req: Request, res: Response, next: NextFunction) {
