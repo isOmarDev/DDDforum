544cb8831fc4dcff0c947eee873df2d1b70ead14
omar kamel <isomardev@gmail.com>
2026-04-23 19:27:07 +0200

    feat: add Config class for environment and script management

---
+++ packages/backend/src/shared/config/index.ts
@@ -0,0 +1,13 @@
+export type Environment = 'development' | 'production' | 'staging' | 'ci';
+
+export type Script = 'start' | 'test:unit' | 'test:e2e' | 'test:infra';
+
+export class Config {
+  public env: Environment;
+  public script: Script;
+
+  constructor(script: Script) {
+    this.env = (process.env.NODE_ENV as Environment) || 'development';
+    this.script = script;
+  }
+}
