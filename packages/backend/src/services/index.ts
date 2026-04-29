4705758c6bc9b01f001bb299580cd10e4b0fca7f
omar kamel <isomardev@gmail.com>
2026-04-29 17:37:32 +0300

    refactor: move  controllers, services, and DTOs to its respective modular folder

--- packages/backend/src/services/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './user-service';
-export * from './marketing-service';
-export * from './post-service';
-export * from './fake-mail-service';
--- packages/backend/src/services/post-service.ts
+++
@@ -1,9 +0,0 @@
-import { PostRepo } from '../persistence/post-repo';
-
-export class PostService {
-  constructor(private postRepo: PostRepo) {}
-
-  public async getPosts(filters?: {}) {
-    return await this.postRepo.findAll(filters);
-  }
-}
