a5eba60283a427d47493b83ae5069b5f4b2e0327
omar kamel <isomardev@gmail.com>
2026-04-30 15:58:51 +0300

    refactor: add GlobalErrorHandler for centralized error handling

---
+++ packages/backend/src/shared/errors/global-error-handler.ts
@@ -0,0 +1,43 @@
+import type { Request, Response, NextFunction } from 'express';
+
+import { InvalidRequestBodyException } from './exceptions';
+import { ErrorException } from './error-exception-types';
+import type { ApiResponse, GenericErrors } from '@dddforum/shared/types';
+
+export type ErrorHandler = (
+  error: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => void;
+
+class GlobalErrorHandler {
+  static handle(
+    error: Error,
+    _req: Request,
+    res: Response,
+    _next: NextFunction,
+  ) {
+    let responseBody: ApiResponse<undefined, GenericErrors>;
+
+    if (error instanceof InvalidRequestBodyException) {
+      responseBody = {
+        success: false,
+        data: undefined,
+        error: { code: ErrorException.ValidationError, message: error.message },
+      };
+
+      return res.status(400).json(responseBody);
+    }
+
+    responseBody = {
+      success: false,
+      data: undefined,
+      error: { code: ErrorException.ServerError, message: error.message },
+    };
+
+    return res.status(500).json(responseBody);
+  }
+}
+
+export default GlobalErrorHandler;
