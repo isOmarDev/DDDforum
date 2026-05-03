3316b9ba126c24a2719e79744c6a7a954d9b521c
omar kamel <isomardev@gmail.com>
2026-05-03 16:22:43 +0300

    fix: update posts state type and correct response data structure in MainPage

--- packages/frontend/src/pages/mainPage.tsx
+++ packages/frontend/src/pages/mainPage.tsx
@@ -3,14 +3,16 @@ import { Layout } from '../components/layout';
 import { PostsList } from '../components/postsList';
 import { PostsViewSwitcher } from '../components/postsViewSwitcher';
 import { api } from '../api';
+import { Post } from '@dddforum/shared/api/posts';
 
 export const MainPage = () => {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState<Post[]>([]);
+
   const loadPosts = async () => {
     try {
       const response = await api.posts.getPosts();
 
-      setPosts(response.data.data.posts);
+      setPosts(response.data!.posts);
     } catch (err) {
       console.log(err);
     }
